2fc025fae393ab13f386ecf5b6f281d927244eeb
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2025-06-17 09:28:34 +0500

    feat: Update `untilBuild` to `243.*`
    This commit updates the `untilBuild` property in the `patchPluginXml` configuration from `242.*` to `243.*` in the `build.gradle.kts` file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.naviy.uz"
-version = "1.1-SNAPSHOT"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("243.*")
     }
 
     signPlugin {
