f882443910569e63c7a133dfe8ba0d6ca9bbfe29
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2025-11-15 11:48:01 +0500

    feat: Update version to 1.1.7 and add new Samsung device configurations in deviceStreaming.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.naviy.uz"
-version = "1.1.6"
+version = "1.1.7"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-//        untilBuild.set("252.*")
+        untilBuild.set("252.*")
     }
 
     signPlugin {
