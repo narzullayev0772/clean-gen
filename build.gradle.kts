f327cf2e1c6301fd161710fc1750c33b69b0b3a8
Alisher Narzullayev <alishernarzullayev7777@gmail.com>
2026-05-11 09:33:19 +0500

    feat: Versiyani 1.1.8 ga yangilash va Samsung qurilmalarining yangi konfiguratsiyalarini qo'shish

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.naviy.uz"
-version = "1.1.7"
+version = "1.1.8"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("252.*")
     }
 
     signPlugin {
